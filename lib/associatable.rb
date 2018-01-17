f56610d05fa1529ff82c8af5d983cd1a3a54b3f3
Daniel Appel <dan.s.appel@gmail.com>
2018-01-16 20:29:26 -0500

    has one

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -20,8 +20,10 @@ class AssocOptions
 end
 
 class BelongsToOptions < AssocOptions
-  def initialize(name, options = {})
-    options = {foreign_key: "#{name}_id".to_sym, class_name: name.to_s.camelcase, primary_key: :id}.merge(options)
+  def initialize(name, self_class_name, options = {})
+    options = {foreign_key: "#{name}_id".to_sym, class_name: name.to_s.capitalize.camelcase, primary_key: :id}.merge(options)
+    options[:foreign_key] = "#{self_class_name.constantize.table_name}.#{options[:foreign_key]}"
+    options[:primary_key] = "#{options[:class_name].constantize.table_name}.#{options[:primary_key]}"
     @name = name
     @foreign_key = options[:foreign_key]
     @class_name = options[:class_name]
@@ -31,7 +33,9 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    options = {foreign_key: "#{self_class_name.downcase}_id".to_sym, class_name: name.to_s.singularize.camelcase, primary_key: :id}.merge(options)
+    options = {foreign_key: "#{self_class_name.downcase}_id".to_sym, class_name: name.to_s.singularize.capitalize.camelcase, primary_key: :id}.merge(options)
+    options[:foreign_key] = "#{options[:class_name].constantize.table_name}.#{options[:foreign_key]}"
+    options[:primary_key] = "#{self_class_name.constantize.table_name}.#{options[:primary_key]}"
     @name = name
     @foreign_key = options[:foreign_key]
     @class_name = options[:class_name]
@@ -49,18 +53,20 @@ module Associatable
   end
 
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    options = BelongsToOptions.new(name, self.name, options)
     assoc_options(options)
+    key = options.foreign_key.split(".")[1]
     define_method(options.name) do
-      options.class_name.to_s.capitalize.constantize.where({options.primary_key=>self.send(options.foreign_key)}).first
+      options.class_name.to_s.capitalize.constantize.where({options.primary_key=>self.send(key)}).first
     end
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, self.class.name, options)
+    options = HasManyOptions.new(name, self.name, options)
     assoc_options(options)
+    key = options.primary_key.split(".")[1]
     define_method(options.name) do
-      options.class_name.to_s.capitalize.constantize.where({options.foreign_key=>self.send(options.primary_key)})
+      options.class_name.to_s.capitalize.constantize.where({options.foreign_key=>self.send(key)})
     end
   end
 
@@ -74,8 +80,8 @@ module Associatable
       source = through_class.assoc_ops.select{|op| source_method == op.name}.first
       if source
         source_class = source.model_class
-        "#{self.table_name} JOIN #{through_class.table_name} ON #{self.table_name}.#{through.primary_key} = #{through_class.table_name}.#{through.foreign_key}" +
-        " JOIN #{source_class.table_name} ON #{through_class.table_name}.#{source.primary_key} = #{source_class.table_name}.#{source.foreign_key}"
+        "#{self.table_name} JOIN #{through_class.table_name} ON #{through.primary_key} = #{through.foreign_key}" +
+        " JOIN #{source_class.table_name} ON #{source.primary_key} = #{source.foreign_key}"
       else
         "#{self.table_name} JOIN #{through_class.table_name} ON #{self.table_name}.#{through.primary_key} = #{through_class.table_name}.#{through.foreign_key}" +
         " JOIN #{through_class.send("through_from_#{source}".to_sym)}"
@@ -83,24 +89,6 @@ module Associatable
     }
 
   end
-
-  # def has_many_through_where(name, through_method, source_method)
-  #
-  #   define_singleton_method("through_where_#{name}") {
-  #     through = assoc_ops.select {|op| through_method == op.name}.first
-  #     through_class = through.model_class
-  #     source = through_class.assoc_ops.select{|op| source_method == op.name}.first
-  #     if source
-  #       source_class = source.model_class
-  #       "#{self.table_name}.#{through.primary_key} = #{through_class.table_name}.#{through.foreign_key}" +
-  #       " AND #{through_class.table_name}.#{source.primary_key} = #{source_class.table_name}.#{source.foreign_key}"
-  #     else
-  #       "#{self.table_name}.#{through.primary_key} = #{through_class.table_name}.#{through.foreign_key}" +
-  #       " AND #{through_class.send("through_where_#{source}".to_sym)}"
-  #     end
-  #   }
-  #
-  # end
 
   def has_many_through_select(name, through_method, source_method)
 
@@ -118,11 +106,25 @@ module Associatable
 
   end
 
+  def has_many_through_class_name(name, through_method, source_method)
+
+    define_singleton_method("through_class_name_#{name}") {
+      through = assoc_ops.select {|op| through_method == op.name}.first
+      through_class = through.model_class
+      source = through_class.assoc_ops.select{|op| source_method == op.name}.first
+      if source
+        source_class = source.model_class
+      else
+        through_class.send("through_class_name_#{source_method}".to_sym)
+      end
+    }
+
+  end
 
   def has_many_through(name, through_method, source_method)
     has_many_through_select(name, through_method, source_method)
     has_many_through_from(name, through_method, source_method)
-    has_many_through_where(name, through_method, source_method)
+    has_many_through_class_name(name, through_method, source_method)
 
     define_method(name) {
     instances = DBConnection.execute(<<-SQL)
@@ -134,6 +136,21 @@ module Associatable
 
     instances
     }
+  end
+
+  def has_one_through(name, through_method, source_method)
+    has_many_through(name, through_method, source_method)
+    define_method(name) {
+      instances = DBConnection.execute(<<-SQL)
+      SELECT
+      #{self.class.send("through_select_#{name}".to_sym)}
+      FROM
+      #{self.class.send("through_from_#{name}".to_sym)}
+      SQL
+
+      instances.first
+    }
+
   end
 
 end
